bf89ae4846f7dd6dc7670b69e0bcc221b18a92af
Dan Groze <dangroze@gmail.com>
2019-02-19 21:33:19 +0000

    Partial solution, needs some refactoring

--- app.rb
+++ app.rb
@@ -1,8 +1,36 @@
 require 'sinatra/base'
 
 class Birthday < Sinatra::Base
+
+  enable :sessions
+
   get '/' do
-    "Hello how you doin"
+    erb :index
+  end
+
+  post "/details" do
+    session[:name] = params[:name]
+    session[:day] = params[:day]
+    session[:month] = params[:month]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @name = session[:name]
+    @day = session[:day]
+    @month = session[:month]
+
+    @day = @day.to_i
+    @month = @month.to_i
+
+    start_date = DateTime.now
+    end_date = DateTime.new(DateTime.now.year, @month, @day)
+    if start_date > end_date
+      end_date = DateTime.new(DateTime.now.year + 1, @month, @day)
+    end
+    @difference = (start_date..end_date).count
+
+    erb(:play)
   end
 
   run! if app_file == $0
